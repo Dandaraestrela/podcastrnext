3e4030e4128a7b95f92e28421032509aeb585e31
Dandara Estrela <dandara_estrela@hotmail.com>
2021-04-25 20:18:49 -0300

    Organização do ambiente e primeira página do site

---
+++ src/pages/index.tsx
@@ -0,0 +1,107 @@
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import { format, parseISO } from "date-fns";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+import ptBR from "date-fns/locale/pt-BR";
+import { api } from "../services/api";
+
+import styles from "./home.module.scss";
+
+// aqui poderia ser interface ou type
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  url: string;
+  description: string;
+  duration: string;
+  durationAsString: string;
+  members: string;
+  publishedAt: string;
+};
+
+type HomeProps = {
+  latestEpisodes: Episode[]; // o mesmo que Array<Episode>
+  allEpisodes: Episode[];
+};
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  return (
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+        <ul>
+          {latestEpisodes.map((episode) => {
+            return (
+              // essa key ajuda o react a saber por ex. qual item deletar, criar, atualizar
+              // sem isso ele teria que renderizar todos novamente a cada mudança
+              // e isso resolve um erro exibido no console
+              // Image é um componente existente apenas do next
+              <li key={episode.id}>
+                <Image
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  objectFit="cover"
+                />
+                <div className={styles.episodeDetails}>
+                  <a href="">{episode.title}</a>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="tocar episodio" />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}></section>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.lenght);
+
+  return {
+    props: {
+      // episodes é o que queremos pegar da api (que nesse caso é um server local)
+      latestEpisodes,
+      allEpisodes,
+    },
+    // define quantas vezes a requisição será refeita
+    revalidate: 60 * 60 * 8,
+  };
+};
